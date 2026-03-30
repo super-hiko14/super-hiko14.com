b8906dd726ec344d9dc8bfa63650ec377c8a1d87
高級ジェネ <kokyujene14@gmail.com>
2026-03-31 00:37:22 +0900

    Offline

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'offline-cache-v1';
+const OFFLINE_URL = 'index.html'; 
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.add(OFFLINE_URL);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.match(OFFLINE_URL);
+      })
+    );
+  }
+});
