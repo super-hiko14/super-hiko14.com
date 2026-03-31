a0630bbc4145927986f56e87bb211c39212efc77
高級ジェネ <kokyujene14@gmail.com>
2026-03-31 11:11:21 +0900

    Oldddddss

--- sw.js
+++ sw.js
@@ -1,24 +1,24 @@
-const CACHE_NAME = 'offline-cache-v2';
+const CACHE_NAME = 'offline-cache-v2'; 
 const OFFLINE_URL = './offline.html';
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        OFFLINE_URL,
-        './Jost.ttf',
-        './zenkaku.ttf'
-      ]).catch(err => console.log("一部のファイルがキャッシュできませんでした", err));
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.add(OFFLINE_URL))
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(() => {
-        return caches.match(OFFLINE_URL);
-      })
-    );
-  }
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Old cache deleted:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
